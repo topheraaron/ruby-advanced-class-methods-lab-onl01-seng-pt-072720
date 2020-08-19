564ee5d14a8141c716faff9803c4a9ba57e80283
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-19 04:13:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,12 +29,15 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@all.each do |song|
-      if song.name == name
-        return song
-      end
-    end
+    @@all.find{|x| x.name == name}
   end
+  #def self.find_by_name(name)
+  #  @@all.each do |song|
+    #  if song.name == name
+      #  return song
+    #  end
+    #end
+#  end
 
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
