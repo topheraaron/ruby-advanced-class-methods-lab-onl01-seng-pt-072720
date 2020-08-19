7e9b3b63b73024aefbb0664b856797166df2c5b3
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-19 04:05:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,8 +36,8 @@ class Song
     end
   end
 
-  #def self.find_or_create_by_name(name)
-  #  self.find_by_name(name) || self.create_by_name(name)
-  #end
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
 
 end
